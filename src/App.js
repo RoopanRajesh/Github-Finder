07885409d65a5a55e2639852f2b89b4fc713b4a2
RoopanRajesh <roopanrajesh123@gmail.com>
2020-05-04 13:15:03 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<Fragment>
+				<Navbar />
+			</Fragment>
+		);
+	}
+}
+
+export default App;
